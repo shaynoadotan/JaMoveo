35ada2a36ed7469331d98516c776800b63e2eaf5
shaynoadotan <118252959+shaynoadotan@users.noreply.github.com>
2024-10-15 20:31:11 +0300

    wip

--- Backend/src/middlewares/auth.ts
+++ Backend/src/middlewares/auth.ts
@@ -1,15 +1,15 @@
-import jwt from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
+// import jwt from 'jsonwebtoken';
+// import { Request, Response, NextFunction } from 'express';
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.header('Authorization')?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'Access Denied' });
+// export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//   const token = req.header('Authorization')?.split(' ')[1];
+//   if (!token) return res.status(401).json({ message: 'Access Denied' });
 
-  try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET!);
-    req.user = verified;
-    next();
-  } catch (error) {
-    res.status(400).send('Invalid Token');
-  }
-};
+//   try {
+//     const verified = jwt.verify(token, process.env.JWT_SECRET!);
+//     req.user = verified;
+//     next();
+//   } catch (error) {
+//     res.status(400).send('Invalid Token');
+//   }
+// };
